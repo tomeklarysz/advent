d5e31c9da4ed6937da73491d36b306d24de870fd
tomek.larysz <ttomek.larysz@gmail.com>
2023-12-05 19:48:57 +0100

    corrected, gives right answer

--- 2/1.py
+++ 2/1.py
@@ -8,37 +8,34 @@ for i in f:
     text = ""
     text = i.replace("Game ", "")
     text = text.split(";")
-    number = int(text[0][0])
     if index == 100: text[0] = text[0][4:]
     else: text[0] = text[0][3:]
     done = False
     print(index)
     index += 1
     print(text)
-    
     for x in text:
         x = x.split(",")
+        print(x)
         for y in x:
             if done == True: break
             y = y.split()
-            if int(y[0]) > 11:
+            print(y)
+            if int(y[0]) > 12:
                 if y[1] == 'red':
                     done = True
                     break
-                elif y[1] == 'green' and int(y[0]) > 12:
+                elif y[1] == 'green' and int(y[0]) > 13:
                     done = True
                     break
-                elif y[1] == 'blue' and int(y[0]) > 13:
+                elif y[1] == 'blue' and int(y[0]) > 14:
                     done = True
                     break
         if done == True: break
-        sum += number
-        print("sum: "+str(sum))
-        break
+    if done == True: continue
+    sum += index-1
+    print("sum: "+str(sum))
 
-# print(number)
-
-# print(text)
 
 print(sum)
 f.close()
