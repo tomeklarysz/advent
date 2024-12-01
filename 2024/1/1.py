ff50dac3558de6750dee16409081de8407ca8ddf
tomeklarysz <ttomek.larysz@gmail.com>
2024-12-01 23:10:42 +0100

    historian hysteria part1

---
+++ 2024/1/1.py
@@ -0,0 +1,39 @@
+file = open("input.txt")
+a, b = [], []
+for f in file:
+    line = f.split()
+    a.append(line[0])
+    b.append(line[1])
+
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(list(left[i:]))
+    result.extend(list(right[j:]))
+    return result
+
+def mergesort(arr):
+    length = len(arr)
+    if length == 1:
+        return arr
+    mid = length // 2
+    l = mergesort(arr[:mid])
+    r = mergesort(arr[mid:])
+    return merge(l, r)
+
+a = mergesort(a)
+b = mergesort(b)
+
+# count the distance
+distance = 0
+for i in range(len(a)):
+    distance += abs(int(a[i]) - int(b[i]))
+
+print(distance)
